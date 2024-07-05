dad0a787474bd5a54040118894008f7dd67ceb5f
naoki tooyama <ntooyama.1112@gmail.com>
2024-07-05 17:09:35 +0900

    sec2

---
+++ resources/js/Pages/Auth/InertiaTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+inertiaテストです。<br>
+<a href="/">aタグ経由です</a><br>
+<Link href="/">Linkタグ経由です</Link><br>
+<Link :href="route('inertia.index')">名前付きルートの確認です</Link><br>
+</template>
---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+
+const emitTest = (e) =>console.log(e)
+
+</script>
+
+<template>
+    <GuestLayout>
+        <Label>タイトル</Label>
+        <Input 
+         modelValue="初期値です"
+         @update:modelValue="emitTest"
+         ></Input><br>
+        GuestLayoutに表示する文字
+    </GuestLayout>
+</template>
